1710c260f497145546aeb4c0938d00df147ae2da
AlanDeng7 <alan.rb.deng@gmail.com>
2023-03-08 22:05:39 -0500

    mar 8

---
+++ SurfsUp/app.py
@@ -0,0 +1,118 @@
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+
+app = Flask(__name__)
+
+#home page
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (f"Available routes: <br/>"
+            f"/api/v1.0/precipitation <br/>"
+            f"/api/v1.0/stations <br/>"
+            f"/api/v1.0/tobs <br/>"
+            f"/api/v1.0/<start> <br/>"
+            f"/api/v1.0/<start>/<end> <br/>"
+    )
+
+#precipitation page
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    #date variable 
+    date = dt.datetime(2017,8,23)-dt.timedelta(days=365)
+
+    #query all data
+    results = session.query(measurement.date,measurement.prcp).\
+        filter(func.strftime(measurement.date)>=date).group_by(measurement.date).all()
+
+    session.close()
+
+    #dictionary for data and append to a list 
+    last_year = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["precopitation"] = prcp
+        last_year.append(prcp_dict)
+
+    return jsonify(last_year)
+
+
+#stations page
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    """Return a list of all station names"""
+    station_names = session.query(station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(station_names))
+
+    return jsonify(all_names)
+
+
+#tobs page
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+
+    #date variable
+    date2 = dt.datetime(2017,8,18)-dt.timedelta(days=365)
+
+    #query all data
+    year_temperature_active = session.query(measurement.date,measurement.tobs).\
+        filter(measurement.station == "USC00519281").filter(measurement.date > date2).all()
+
+    session.close()
+
+    #dictionary for data and append to a list 
+    last_year_active = []
+    for date, temp in year_temperature_active:
+        temp_dict = {}
+        temp_dict["date"] = date
+        temp_dict["temperature"] = temp
+        last_year_active.append(temp_dict)
+
+    return jsonify(last_year_active)
+
+
+
+@app.route("/api/v1.0/<start>")
+def start():
+    print("Server received request for 'start' page...")
+    return "Welcome to my 'About' page!"
+
+@app.route("/api/v1.0/<start>/<end>")
+def end():
+    print("Server received request for 'end' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
